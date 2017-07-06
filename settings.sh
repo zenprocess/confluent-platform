5654f54f2aae7c8fc7b6b33727f254e53f2843de
Zen Process <dev@zen-process.com>
2017-07-06 10:14:02 +0100

    pump up versions of products

--- settings.sh
+++ settings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-: ${SCALA_VERSIONS:="2.10 2.11"}
-: ${DEFAULT_SCALA_VERSION:="2.11"}
-: ${CONFLUENT_PLATFORM_VERSION:="3.0.0"}
-: ${KAFKA_VERSION:="0.10.0.0-cp1"}
-: ${ZOOKEEPER_VERSION:="3.4.6-cp1"}
+: ${SCALA_VERSIONS:="2.11.11 2.12.2"}
+: ${DEFAULT_SCALA_VERSION:="2.11.11"}
+: ${CONFLUENT_PLATFORM_VERSION:="3.2.2"}
+: ${KAFKA_VERSION:="0.10.2.0"}
+: ${ZOOKEEPER_VERSION:="3.4.10"}
 : ${DOCKER_BUILD_OPTS:="--rm=true "}
 : ${DOCKER_TAG_OPTS:="-f "}
-: ${PACKAGE_URL:="http://packages.confluent.io/archive/3.0"}
+: ${PACKAGE_URL:="http://packages.confluent.io/archive/3.2.2"}
 
 #PRIVATE_REPOSITORY=""
 #PUSH_TO_DOCKER_HUB=
